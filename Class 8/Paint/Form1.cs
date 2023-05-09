0ad5822620d01e05a0b951b0c8db399aa3d6d4ab
Henry Brookland (1000112868) <BROOHA1@student.op.ac.nz>
2023-05-10 09:53:44 +1200

    aded code to files

--- Class 8/Paint/Form1.cs
+++ Class 8/Paint/Form1.cs
@@ -1,10 +1,39 @@
+using System.Drawing;
+
 namespace Paint
 {
     public partial class Form1 : Form
     {
+        private Graphics graphics;
         public Form1()
         {
             InitializeComponent();
+            graphics = CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Left += 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top += 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox3.Top += 10;
         }
     }
 }
